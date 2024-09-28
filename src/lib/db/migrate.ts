8daddc59bc531cde052cd4553e3b4ee0ee7ccbda
Jviguy <jviguytwo2@gmail.com>
2024-09-28 00:39:42 -0400

    db migrations code

---
+++ src/lib/db/migrate.ts
@@ -0,0 +1,11 @@
+import { db } from "./db";
+import { migrate as migratePostgres } from "drizzle-orm/aws-data-api/pg/migrator";
+
+export const migrate = async (path: string) => {
+    console.log("Running migrations...");
+    await migratePostgres(db, { migrationsFolder: path });
+    console.log("Migrations completed.");
+};
+
+// @ts-expect-error - this is fine to do.
+await migrate("../../../drizzle/migrations/");
